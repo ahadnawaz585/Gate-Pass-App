a2b3c25a415d094f6eb904a60335b9e1bf1c6eed
ahadnawaz585 <ahadnawaz585@gmail.com>
2025-04-25 23:50:12 +0500

    Remove all JS files

--- src/enums/schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Status = void 0;
-var Status;
-(function (Status) {
-    Status["PENDING"] = "pending";
-    Status["APPROVED"] = "approved";
-    Status["CANCELLED"] = "cancelled";
-})(Status || (exports.Status = Status = {}));
